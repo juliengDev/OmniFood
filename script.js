712a870bad97dd069eab8a232404b5e2f066ffb5
JulienG <juliengilbert.dev@gmail.com>
2023-10-08 20:05:08 +0200

    Smooth scrooling animation

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ yearEl.textContent = currentYear;
 burgerMenuIcon.addEventListener("click", () => {
   header.classList.toggle("nav-open");
 });
+
+// Smooth scrooling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault;
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEL = document.querySelector(href);
+      sectionEL.scrollIntoView({ behavior: "smooth" });
+    }
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
