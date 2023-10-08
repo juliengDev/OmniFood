801fb9a8c58d85effcfc8ed25fafc15c8784b2c2
JulienG <juliengilbert.dev@gmail.com>
2023-10-08 19:17:37 +0200

    Anchor links added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const burgerMenuIcon = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
 burgerMenuIcon.addEventListener("click", () => {
   header.classList.toggle("nav-open");
 });
